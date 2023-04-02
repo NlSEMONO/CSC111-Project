28e2c5b0f9db01a9df8a7758d8c810086d272740
NlSEMONO <fmadarang2542@hotmail.com>
2023-04-02 13:44:01 -0400

    Fixed another bug involving TreePlayer making invalid mvoes

--- TreePlayer.py
+++ TreePlayer.py
@@ -98,7 +98,8 @@ class TreePlayer(Player):
         if self.exploring:
             move_type = random.choice(self.choices)
             while (move_type in {CHECK_CODE, BET_CODE} and game_state.last_bet > 0) or \
-                    (move_type in {CALL_CODE, RAISE_CODE} and game_state.last_bet == 0): # reroll the move if invalid
+                    (move_type in {CALL_CODE, RAISE_CODE} and game_state.last_bet == 0) or \
+                    (move_type == RAISE_CODE and self.has_raised): # reroll the move if invalid
                 move_type = random.choice(self.choices)
             return self._final_decision(game_state, move_type)
         return (0, 0)
